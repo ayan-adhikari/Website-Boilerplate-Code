feaa5ef025e829ef98ba765d86e1d3080c2f5049
Ayan Adhikari <ayanadhi2022@gmail.com>
2026-03-18 16:31:21 +0800

    Feat: complete loading screen

--- script.js
+++ script.js
@@ -2,13 +2,17 @@
 window.addEventListener(`load`, pageLoaded)
 function pageLoaded()
 {
-    console.log(`Page loaded`)
-    /*
-    document.getElementById("page-content").classList.remove("hidden")
-    document.getElementById("loading-screen").classList.add("hidden")
-    */
+  console.log(`Page loaded`)
+  //Hide main loading screen and wrapper
+  document.getElementById("loading-screen").classList.add("hidden")
+  document.getElementById("loader-wrapper").classList.add("hidden")
+  //Remove flexbox so they dont take up space
+  document.getElementById("loading-screen").classList.remove("flexbox")
+  document.getElementById("loader-wrapper").classList.remove("flexbox")
+  //Show main page
+  document.getElementById("page-content").classList.remove("hidden")
 }
-//Function for generating numbers between min to max (includinng min and max)
+//Function for generating numbers between min to max (including min and max)
 function generateRandomNumber(min, max) 
 {
   return Math.floor(Math.random() * (max - min + 1)) + min;
