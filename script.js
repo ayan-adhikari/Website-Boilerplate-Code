a352f5c1225087bc45df454de5879307023b981e
Ayan Adhikari <ayanadhi2022@gmail.com>
2026-03-21 07:16:16 +0800

    Fix: remove all loading screen issues:

--- script.js
+++ script.js
@@ -2,16 +2,24 @@
 window.addEventListener(`load`, pageLoaded)
 function pageLoaded()
 {
-  console.log(`Page loaded`)
-  //Hide main loading screen and wrapper
-  document.getElementById("loading-screen").classList.add("hidden")
-  document.getElementById("loader-wrapper").classList.add("hidden")
-  //Remove flexbox so they dont take up space
-  document.getElementById("loading-screen").classList.remove("flexbox")
-  document.getElementById("loader-wrapper").classList.remove("flexbox")
-  //Show main page
-  document.getElementById("page-content").classList.remove("hidden")
-}
+  setTimeout(() => {
+    console.log(`Page loaded`)
+    //Hide main loading screen and wrapper
+    document.getElementById("loading-screen").classList.add("hidden")
+    document.getElementById("loader-wrapper").classList.add("hidden")
+    //Remove flexbox so they dont take up space
+    document.getElementById("loading-screen").classList.remove("flexbox")
+    document.getElementById("loader-wrapper").classList.remove("flexbox")
+    document.getElementById("outer-loader").classList.remove("flexbox")
+    document.getElementById("middle-loader").classList.remove("flexbox")
+    document.getElementById("inner-loader").classList.remove("flexbox")
+    //Show main page
+    document.getElementById("page-content").classList.remove("hidden")
+  }, 1); /*This is the delay which can be increased for testing and is 1ms by default*/
+    
+  
+  
+  }
 //Function for generating numbers between min to max (including min and max)
 function generateRandomNumber(min, max) 
 {
